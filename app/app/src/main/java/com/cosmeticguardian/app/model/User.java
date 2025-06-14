fd8480093e0a0acb8e713488a7af94d24f60ab8c
jesusdangerous <aleksandrzonov641@gmail.com>
2025-06-14 16:23:18 +0500

    Add user model

---
+++ app/app/src/main/java/com/cosmeticguardian/app/model/User.java
@@ -0,0 +1,33 @@
+package com.cosmeticguardian.app.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+    private String telegramId;
+
+    private LocalDate birthDate;
+
+    @ElementCollection
+    private List<String> allergens;
+
+    @ElementCollection
+    private List<String> skinProblems;
+
+    private boolean emailConfirmed;
+}
