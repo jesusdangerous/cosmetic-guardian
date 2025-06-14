d14da08ded8c03a489d0f19b41523bf6cf58ec5e
jesusdangerous <aleksandrzonov641@gmail.com>
2025-06-14 16:39:23 +0500

    Add user repository

---
+++ app/app/src/main/java/com/cosmeticguardian/app/repo/UserRepository.java
@@ -0,0 +1,13 @@
+package com.cosmeticguardian.app.repo;
+
+import com.cosmeticguardian.app.model.User;
+import jakarta.validation.constraints.Email;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByTelegramId(String telegramId);
+
+    Optional<User> findByEmail(@Email String email);
+}
